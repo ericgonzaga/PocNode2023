3f215f14979657ad93813e11e72a29cac4c35029
Eric Gonzaga <ericgonzaga@pbh.gov.br>
2023-03-20 09:44:17 -0300

    testing husky

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ server.listen(express_port, () => {
     console.log(`Express server running at http://localhost:${express_port}`);
 });
 
+const testing = 'testing';
+
 // const gql_port = process.env.GQL_PORT ? Number(process.env.GQL_PORT) : 4001;
 // bootstrap(gql_port).then(({ url }) => {
 //     console.log(`GraphQL server running at ${url}`);
