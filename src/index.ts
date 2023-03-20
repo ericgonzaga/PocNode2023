12114c8324858769df8f16875f60c586e0427ee9
Eric Gonzaga <ericgonzaga@pbh.gov.br>
2023-03-20 08:53:29 -0300

    testing husky

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { server } from '@/server';
-// import { bootstrap } from './graphql';
+import { bootstrap } from './graphql';
 
 import 'dotenv/config';
 
 const express_port = process.env.EXPRESS_PORT || 4000;
 server.listen(express_port, () => {
-    console.log(`Server running at http://localhost:${express_port}`);
+    console.log(`Express server running at http://localhost:${express_port}!`);
 });
 
-// const gql_port = process.env.GQL_PORT ? Number(process.env.GQL_PORT) : 4001;
-// bootstrap(gql_port).then(({ url }) => {
-//     console.log(`GraphQL server running at ${url}`);
-// });
+const gql_port = process.env.GQL_PORT ? Number(process.env.GQL_PORT) : 4001;
+bootstrap(gql_port).then(({ url }) => {
+    console.log(`GraphQL server running at ${url}!`);
+});
